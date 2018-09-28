13531ceb87368ab3d7803bca8c646fbb23c8b01a
deadman96385 <seanhoyt963@gmail.com>
2018-09-27 23:51:19 -0500

    Add some props from stock

--- system_prop.mk
+++ system_prop.mk
@@ -7,5 +7,41 @@ PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     audio_hal.force_voice_config=wide
 
+# Dalvik/Art
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=128m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.image-dex2oat-filter=speed \
+    dalvik.vm.isa.arm64.variant=generic \
+    dalvik.vm.isa.arm64.features=default \
+    dalvik.vm.isa.arm.variant=cortex-a15 \
+    dalvik.vm.isa.arm.features=default
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196609 \
+    debug.hwc.force_gpu=1
+
+# Hwui
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=24 \
+    ro.hwui.layer_cache_size=16 \
+    ro.hwui.path_cache_size=4 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.shape_cache_size=1 \
+    ro.hwui.gradient_cache_size=0.5 \
+    ro.hwui.drop_shadow_cache_size=2 \
+    ro.hwui.r_buffer_cache_size=2 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=512 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024 \
+
 # Wifi
-wifi.interface=wlan0
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0
