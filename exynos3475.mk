cf0b5a7f5250530c8ca1d1ff1f80624a158f4f18
Ryan Cueva <51128072+ryancueva282@users.noreply.github.com>
2024-03-09 20:20:26 -0600

    Update exynos3475.mk

--- exynos3475.mk
+++ exynos3475.mk
@@ -14,13 +14,10 @@
 # limitations under the License.
 #
 
-# Inherit from common
-$(call inherit-product-if-exists, device/samsung/qcom-common/qcom-common.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Include proprietary blobs
-$(call inherit-product-if-exists, vendor/samsung/msm8916-common/msm8916-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/exynos3475-common/exynos3475-common-vendor.mk)
 
 LOCAL_PATH := device/samsung/msm8916-common
 
